fb6c246d602064471e0ff0f0b677392c75bafb7d
Akhil Garg <Akhil.Garg@fisglobal.com>
2019-12-26 16:46:44 +0530

    added zul gateway

---
+++ netflix-zuul-api-gateway/src/main/java/com/fix/netflixzuulapigateway/filter/ZuulfilterLogger.java
@@ -0,0 +1,41 @@
+package com.fix.netflixzuulapigateway.filter;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class ZuulfilterLogger extends ZuulFilter {
+
+	private static Logger logger= LoggerFactory.getLogger(ZuulfilterLogger.class);
+	
+	
+	@Override
+	public boolean shouldFilter() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		HttpServletRequest req= RequestContext.getCurrentContext().getRequest();
+		logger.info("request-> {} request uri-> {}",req,req.getRequestURI());
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 0;
+	}
+}
